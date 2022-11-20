c49e2da7f4e3149bcccaf97b6b0d885f88b42f75
Thodler <neoskywalker01@gmail.com>
2022-11-20 13:12:35 +0100

    [ADD] Models User et Adresse

---
+++ src/app/_models/Address.ts
@@ -0,0 +1,8 @@
+interface Address {
+  city:string
+  country: string
+  state: string
+  street_address: string
+  street_name: string
+  zip_code: string
+}
---
+++ src/app/_models/User.ts
@@ -0,0 +1,7 @@
+export class User {
+  avatar!: string
+  first_name!:string
+  last_name!: string
+  date_of_birth!: Date
+  address!: Address
+}
