fe3c5c94fee7e4aec991b24118ead49045e6992b
Thodler <neoskywalker01@gmail.com>
2022-11-20 13:13:51 +0100

    [UPD] Ajout de la méthode GET pour recupérer un User Random

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../_models/User";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  url = "https://random-data-api.com/api/v2/users"
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUser():Observable<User>
+  {
+    return this.http.get<User>(this.url)
+  }
 }
